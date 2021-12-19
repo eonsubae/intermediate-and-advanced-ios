7ab4e98b5800d9dc64ee4f3f842230f9e6947e0e
eonsubae <eonsubae@naver.com>
2021-12-19 12:57:59 +0900

    Example 1 - Constraints in Generics(findIndex)

--- generics/why-generics.playground/Contents.swift
+++ generics/why-generics.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/*
 let numbers = [1,2,3,5,6,7,10]
 
 func firstLast(_ numbers: [Int]) -> (Int, Int) {
@@ -50,3 +51,22 @@ let (first5, last5) = firstLastGeneric(movies)
 
 print(first5)
 print(last5)
+*/
+
+struct Movie: Equatable {
+    let name: String
+}
+
+let numbers = [1,2,3,5,6,7,10]
+let names = ["Alex", "John", "Mary", "Steve"]
+let movies = [Movie(name: "Batman"), Movie(name: "Superman"), Movie(name: "Spiderman")]
+
+func findIndex<T: Equatable>(from list: [T], valueToFind: T) -> Int? {
+    list.firstIndex { $0 == valueToFind }
+}
+
+print(findIndex(from: numbers, valueToFind: 7))
+print(findIndex(from: names, valueToFind: "Mary"))
+
+let batman = Movie(name: "Batman")
+print(findIndex(from: movies, valueToFind: batman))
